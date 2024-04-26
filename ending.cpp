35fa5917074b1eba00159929796b46f9062c6976
Samuel Osondu <samuelosondu.py@gmail.com>
2024-04-26 16:30:41 +0100

    exercise

---
+++ ending.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int x;
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+    std::cout << "Double of " << x << " is: " << x * 2 << "\n";
+    std::cout << "Tripple of " << x << " is: " << x * 3;
+ }
